cd7c2d237b7e463edf26f17a398e4f54bde5537c
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-01 16:55:33 -0700

    moves task to finished list when name is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,51 @@ $(".task-form").submit(function(event){
 	var inputtedDescription = $(".new-description").val();
 	
 	var newTask = { name: inputtedName, description: inputtedDescription}
-	$(".task-list").append("<li class='task'>" + newTask.name + "</li>");
+	$(".task-list").append("<li class='task'>" + newTask.name +  "</li>" + "<span class='btn btn-default see-info " + newTask.name + "'>See details</span>");
 	
-	$(".task").last().click(function(){	
-		$(this).toggleClass('selected');
-			
+	$(".see-info").last().click(function(){				
 		$(".task-info").show();
 		
 		$(".name").text(newTask.name);
 		$(".description").text(newTask.description);
-		$(".button").html('<span class="btn btn-default remove-task">Task Complete!</span>');
 		
-		
-		$(".remove-task").click(function(){
-			$('.done-task-list').append($('.selected').removeClass('selected task'));
-			$(".task-info").hide();
-
-		});
-			
 	});
 	
+	$(".task").click(function(){
+		$(this).addClass('selected');
+		var className = $(this).text();
+		$("."+className).remove();
+				
+		
+		$('.done-task-list').append($('.selected').removeClass('selected task'));
+		$(".task-info").hide();
+
+	});
 
 	$(".new-name").val("")
 	$(".new-description").val("")
 
 });
 
+/*
+  $('.task-form').submit(function(event){
+    event.preventDefault();
+
+    var inputName = $(".new-name").val();
+    var inputDescription = $(".new-description").val();
+
+    var newTask = { taskName: inputName, taskDescription: inputDescription };
+
+  $(".task-list").append("<p><span class='task-incomplete'>" + newTask.taskName + " - " + newTask.taskDescription + "</p>");
+
+  $(".task-incomplete").last().click(function() {
+    this.remove()
+    $(".done-task-list").append("<p><span class='task-complete'>Task: " + newTask.taskName + "</p>");
+
+    });
+
+    $(".new-name").val("");
+    $(".new-description").val("");
+
+  });
+*/
