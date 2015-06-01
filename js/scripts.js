0cdff85cfd2b111886b6c3150d322cf5605b77bb
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-01 12:48:36 -0700

    tasks list when added, form clears after every submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(".task-form").submit(function(event){
 	event.preventDefault();
 	
+	var newTask = $(".new-task").val()
+	
+	$(".task-list").append("<li>" + newTask + "</li>")
+	
+	
+	
+	$(".new-task").val("")
 });
+
