0d8d1728492a11e5d033de5f2431faade0c62dc1
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-01 14:37:32 -0700

    will let you add more tasks after deleting

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,27 @@ $(".task-form").submit(function(event){
 	var inputtedDescription = $(".new-description").val();
 	
 	var newTask = { name: inputtedName, description: inputtedDescription}
-	
 	$(".task-list").append("<li class='task'>" + newTask.name + "</li>");
 	
-	$(".task").last().click(function(){
+	$(".task").last().click(function(){	
 		$(this).toggleClass('selected');
-		
+			
 		$(".task-info").show();
 		
 		$(".name").text(newTask.name);
 		$(".description").text(newTask.description);
+		$(".button").html('<span class="btn btn-default remove-task">Task Complete!</span>');
+		
 		
 		$(".remove-task").click(function(){
-			$('.done-task-list').append($('.task-list .selected').removeClass('selected'));
+			$('.done-task-list').append($('.selected').removeClass('selected task'));
 			$(".task-info").hide();
 
-			
 		});
-		
+			
 	});
 	
+
 	$(".new-name").val("")
 	$(".new-description").val("")
 
