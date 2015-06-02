1b93a6705b74c924008349d9488b97d2a5b46909
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-01 17:40:11 -0700

    will remove task from completed list when you click on task name

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,17 @@ $(".task-form").submit(function(event){
 	var inputtedDescription = $(".new-description").val();
 	
 	var newTask = { name: inputtedName.replace(/[^a-zA-Z0-9]+/g, ' '), description: inputtedDescription}
-	$(".task-list").append("<li class='task'>" + newTask.name +  "</li>" + "<span class='btn btn-default see-info " + newTask.name + "'>See details</span>");
+	$(".task-list").append("<li class='task'>" + 
+								newTask.name +  
+							"</li>" + 
+							"<span class='btn btn-default see-info " + newTask.name + "'>See details" + 
+							"</span>");
 	
 	$(".see-info").last().click(function(){				
 		$(".task-info").show();
 		
 		$(".name").text(newTask.name);
 		$(".description").text(newTask.description);
-		
 		
 	});
 	
@@ -24,11 +27,10 @@ $(".task-form").submit(function(event){
 				
 		$('.done-task-list').append($('.selected').removeClass('selected task').addClass('done-task'));
 		$(".task-info").hide();
-
-	});
-	
-	$(".done-task").click(function(){
-		$(this).remove();
+		
+		$(".done-task").click(function(){
+			this.remove();
+		});
 	});
 
 	$(".new-name").val("")
