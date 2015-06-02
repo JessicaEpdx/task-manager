9e76cf5a463903c4cd3638d6751aecfb825f35f1
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-01 19:09:20 -0700

    everything working

--- js/scripts.js
+++ js/scripts.js
@@ -8,26 +8,23 @@ $(".task-form").submit(function(event){
 	var newTask = { taskName: inputtedName.replace(/[^a-zA-Z0-9]+/g, ' '), description: inputtedDescription}
 	
 	if(inputtedName !== ""){
-			$(".task-list").append("<li class='task'>" + 
-								newTask.taskName +  
-							"</li>" + 
-							"<span class='btn btn-info see-info " + newTask.taskName + "'>DESCRIPTION" + 
-							"</span>");
-		
-	}
-
-	
-	
-	
-	$(".see-info").last().click(function(){				
+		$(".task-list").append("<li class='task'>" + 
+							newTask.taskName +  
+						"</li>" + 
+						"<span class='btn btn-info see-info " + newTask.taskName + "'>DESCRIPTION" + 
+						"</span>");
+						
+		$(".see-info").last().click(function(){				
 		$(".task-info").show();
 		
 		$(".name").text(newTask.taskName);
 		$(".description").text(newTask.description);
 		
 	});
+
+	};
 	
-	
+		
 	
 	$(".task").click(function(){
 		$(this).addClass('selected');
