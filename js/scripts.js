71866a27a76614bbd5e353f2d1a990dea588cdcc
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-01 21:40:53 -0700

    allow users to input task name with multiple words

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,54 @@
-$(".task-form").submit(function(event){
+var submitForm = function(){$(".task-form").submit(function(event){
 	event.preventDefault();
-	
 	
 	var inputtedName = $(".new-name").val();
 	var inputtedDescription = $(".new-description").val();
-	console.log(inputtedName);
-	var newTask = { taskName: inputtedName.replace(/[^a-zA-Z0-9]+/g, ' '), description: inputtedDescription}
+	var newTask = { taskName: inputtedName.replace(/[^a-zA-Z0-9]+/g, ' '), description: inputtedDescription, id:null}
+	var counter = 0
 	
-	if(inputtedName !== ""){
-		$(".task-list").append("<li class='task'>" + 
-							newTask.taskName +  
-						"</li>" + 
-						"<span class='btn btn-info see-info " + newTask.taskName + "'>DESCRIPTION" + 
-						"</span>");
-						
-		$(".see-info").last().click(function(){				
-			$(".task-info").show();
+	var taskAppendToList = function(){
+		if(inputtedName !== ""){	
 			
-			$(".name").text(newTask.taskName);
-			$(".description").text(newTask.description);
+			console.log(counter)		
+			
+			$(".task-list").append("<li class='task'>" + 
+										newTask.taskName +  
+									"</li>" + 
+									"<span class='btn btn-info see-info " + newTask.taskName.replace(/\s+/g, "-")+ "'>DESCRIPTION" + 
+									"</span>");
+									
+			$(".see-info").last().click(function(){				
+				$(".task-info").show();
+				
+				$(".name").text(newTask.taskName);
+				$(".description").text(newTask.description);
+			
+			});
+		};
+	
+	};
 		
-		});
+	taskAppendToList();
 
+		
+	var taskClick= function(){
+		$(".task").click(function(){		
+			var className = $(this).text();
+			$("."+className.replace(/\s+/g, "-")).remove();
+					
+			$('.done-task-list').append($(this).removeClass('task').addClass('done-task'));
+			
+			$(".task-info").hide();
+			
+			$(".done-task").click(function(){
+				this.remove();
+			});
+		});
 	};
 	
-		
 	
-	$(".task").click(function(){		
-		var className = $(this).text();
-		$("."+className).remove();
-				
-		$('.done-task-list').append($(this).removeClass('task').addClass('done-task'));
-		
-		$(".task-info").hide();
-		
-		$(".done-task").click(function(){
-			this.remove();
-		});
-	});
-
+	
+	taskClick();
 
 
 	$(".new-name").val("")
@@ -46,4 +56,6 @@ $(".task-form").submit(function(event){
 
 });
 
+};
 
+submitForm();
