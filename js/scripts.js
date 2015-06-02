b34dd0e910d7532d9dfdb1b5ca6f405a20060d6e
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-01 17:53:56 -0700

    description button smaller

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,43 @@ $(".task-form").submit(function(event){
 	var inputtedName = $(".new-name").val();
 	var inputtedDescription = $(".new-description").val();
 	
-	var newTask = { name: inputtedName.replace(/[^a-zA-Z0-9]+/g, ' '), description: inputtedDescription}
+	var newTask = { taskName: inputtedName.replace(/[^a-zA-Z0-9]+/g, ' '), description: inputtedDescription}
+	
+	
 	$(".task-list").append("<li class='task'>" + 
-								newTask.name +  
+								newTask.taskName +  
 							"</li>" + 
-							"<span class='btn btn-default see-info " + newTask.name + "'>See details" + 
+							"<span class='btn btn-info see-info " + newTask.taskName + "'>DESCRIPTION" + 
 							"</span>");
+	
+	
 	
 	$(".see-info").last().click(function(){				
 		$(".task-info").show();
 		
-		$(".name").text(newTask.name);
+		$(".name").text(newTask.taskName);
 		$(".description").text(newTask.description);
 		
 	});
 	
+	
+	
 	$(".task").click(function(){
 		$(this).addClass('selected');
+		
 		var className = $(this).text();
 		$("."+className).remove();
 				
 		$('.done-task-list').append($('.selected').removeClass('selected task').addClass('done-task'));
+		
 		$(".task-info").hide();
 		
 		$(".done-task").click(function(){
 			this.remove();
 		});
 	});
+
+
 
 	$(".new-name").val("")
 	$(".new-description").val("")
